881d17c6eade9158e59df4afbc9b4b4424d4256c
ba369 <142502708+ba369@users.noreply.github.com>
2024-05-31 08:12:29 +0530

    Update nlpex4.py

--- nlpex4.py
+++ nlpex4.py
@@ -4,4 +4,4 @@ s ="Machine learning is an important part of AI""and AI is going to become inmpo
 tokens=[token for token in s.split(" ")]
 output =list(ngrams(tokens,2))
 print(output)
-print("Rakesh-211211101037")
+
